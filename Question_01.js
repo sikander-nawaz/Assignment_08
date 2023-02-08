00017b017394f293ca3024402e1683a950b270c3
Khoof_Nak <sikandernawaz35@gmail.com>
2023-02-08 19:19:36 +0500

    Add_Question_01

--- Question_01.js
+++ Question_01.js
@@ -6,11 +6,8 @@ let num2 = 09;
 
 // SOLUTION:
 // USING IF CONDITION 
-if (num1 > num2) {
-                    console.log(`The Maximum Number is ${num1}`);  
-} 
-else{
+if (num1 > num2){
+        console.log(`The Maximum Number is ${num1}`);
+}else{
         console.log(`The Maximum Number is ${num2}`);
 }
-
-
