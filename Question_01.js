c5ecc6f8f830b77fd2bdaf7e9679948b1e6d3b91
Khoof_Nak <sikandernawaz35@gmail.com>
2023-02-08 18:17:05 +0500

    Add_Question_01

---
+++ Question_01.js
@@ -0,0 +1,16 @@
+// Write a js program to find the maximum between two numbers.
+
+// VALUES 
+let num1 = 25;
+let num2 = 09;
+
+// SOLUTION:
+// USING IF CONDITION 
+if (num1 > num2) {
+                    console.log(`The Maximum Number is ${num1}`);  
+} 
+else{
+        console.log(`The Maximum Number is ${num2}`);
+}
+
+
