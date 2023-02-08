1e5219f211ede1413d6c9befbf133351494d6471
Khoof_Nak <sikandernawaz35@gmail.com>
2023-02-08 19:11:59 +0500

    Add_Question_02

--- Question_02.js
+++ Question_02.js
@@ -1,1 +1,20 @@
 // Write a js program to find the maximum between three numbers.
+
+// VALUES 
+let num1 = 25;
+let num2 = 09;
+let num3 = 04;
+
+// SOLUTION:
+// USING ELSE_IF CONDITION 
+if (num1 > num2){
+    if(num1 > num3){
+        console.log(`The Maximum Number is ${num1}`);
+    }
+}else if(num2 > num3){
+    if (num2 > num1) {
+        console.log(`The Maximum Number is ${num2}`);
+    }
+}else{
+    console.log(`The Maximum Number is ${num3}`);
+}
