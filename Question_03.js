fac5f9aaec5c929033475c8a8396d74e20d7a571
Khoof_Nak <sikandernawaz35@gmail.com>
2023-02-09 21:09:13 +0500

    Add_Question_03

--- Question_03.js
+++ Question_03.js
@@ -1,7 +1,14 @@
 // Write a js program to check whether a number is negative, positive or zero.
 
 // VALUES 
-let value = 5;
+let value = 0.25;
 
 // SOLUTION
 // USING ELSE_IF CONDITION
+if (value > 0) {
+    console.log("Positive");
+}else if(value < 0){
+    console.log("Negative");
+}else{
+    console.log("0");
+}
