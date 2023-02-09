3e9cbd2707433a5c35794bce18826fa0e9b2dda7
Khoof_Nak <sikandernawaz35@gmail.com>
2023-02-09 22:35:41 +0500

    Add_Question_04.js

---
+++ Question_04.js
@@ -0,0 +1,12 @@
+// Write a js program to check whether a number is divisible by 5 and 11 or not.
+
+// VALUES 
+let value = 55;
+
+// SOLUTION
+// USING IF CONDITION
+if (value % 5 === 0 && value % 11 === 0) {
+    console.log("Number is divisible.");
+}else{
+    console.log("Number is not divisible.");
+}
