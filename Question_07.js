d7c4f92e11ff9bf60a4c395fad793c28edcf487d
Khoof_Nak <sikandernawaz35@gmail.com>
2023-02-10 21:31:02 +0500

    Add_Question_07

---
+++ Question_07.js
@@ -0,0 +1,12 @@
+// Write a js program to input any alphabet and check whether it is vowel or consonant.
+
+// VALUES 
+let alphabet = "A";
+
+// SOLUTION
+// USING IF CONDITION
+if(alphabet === "a" || alphabet === "A" || alphabet === "e" || alphabet === "E" || alphabet === "i" || alphabet === "I" || alphabet === "o" || alphabet === "O" || alphabet === "u" || alphabet === "U"){
+    console.log("Vowel Alphabet");
+}else{
+    console.log("Constant Alphabet");
+}
