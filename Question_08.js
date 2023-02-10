29de0b7751665fed6dc7d67d4bc9cc97d2d2ccd5
Khoof_Nak <sikandernawaz35@gmail.com>
2023-02-10 22:07:48 +0500

    Add_Question_08

---
+++ Question_08.js
@@ -0,0 +1,14 @@
+// Write a js program to check whether a character is uppercase or lowercase alphabet.
+
+// VALUES 
+let alphabet = "G";
+
+// SOLUTION
+// USING ELSE_IF CONDITION
+if (alphabet >= "a" && alphabet <= "z") {
+    console.log("Lower Case Alphabet");    
+}else if (alphabet >= "A" && alphabet <= "Z") {
+    console.log("Upper Case Alphabet");
+}else{
+    console.log("Bandy Da Putter Ban Ja Tu");
+}
