8b6cc5f32c478062f11be8111921687a3c070468
Khoof_Nak <sikandernawaz35@gmail.com>
2023-02-11 16:59:34 +0500

    Add_Question_09

---
+++ Question_09.js
@@ -0,0 +1,33 @@
+// Write a js program to input the week number and print weekday. 
+
+// VALUES 
+let day = 04;
+
+// SOLUTION:
+// USING SWITCH CONDITION. 
+switch (day) {
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Thursday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    case 6:
+        console.log("Saturday");
+        break;
+    case 7:
+        console.log("Sunday");
+        break;
+    default:
+        console.log("Khopri Tor Salay Ka");
+    break;
+}
