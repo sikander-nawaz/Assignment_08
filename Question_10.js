a64e6667f4c084f7fda01e40ed5ed696143b3c16
Khoof_Nak <sikandernawaz35@gmail.com>
2023-02-11 17:07:17 +0500

    Add_Question_10

---
+++ Question_10.js
@@ -0,0 +1,48 @@
+// Write a js program to input the month number and print the number of days in that month.
+
+// VALUES 
+let month = 04;
+
+// SOLUTION:
+// USING SWITCH CONDITION. 
+switch (month) {
+    case 1:
+        console.log("31");
+        break;
+    case 02:
+        console.log("28");
+        break;
+    case 03:
+        console.log("31");
+        break;
+    case 04:
+        console.log("30");
+        break;
+    case 05:
+        console.log("31");
+        break;
+    case 06:
+        console.log("30");
+        break;
+    case 07:
+        console.log("31");
+        break;
+    case 08:
+        console.log("31");
+        break;
+    case 09:
+        console.log("30");
+        break;
+    case 10:
+        console.log("31");
+        break;
+    case 11:
+        console.log("30");
+        break;
+    case 12:
+        console.log("31");
+        break;
+    default:
+        console.log("Masti Kar Rya Shorya");
+        break;
+}
