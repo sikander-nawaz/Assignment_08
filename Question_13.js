2bf1a927e5d628484242a3c4f98c6bb4343666f9
Khoof_Nak <sikandernawaz35@gmail.com>
2023-02-12 23:00:39 +0500

    Add_Question_13_File

---
+++ Question_13.js
@@ -0,0 +1,4 @@
+// Write a js program to input the basic salary of an employee and calculate its Gross salary according to the following:
+// Basic Salary <= 10000 : HRA = 20%, DA = 80%
+// Basic Salary <= 20000 : HRA = 25%, DA = 90%
+// Basic Salary > 20000 : HRA = 30%, DA = 95%
