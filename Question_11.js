1a1e0812b484a4bb361baa8eaa772c8f43a028e9
Khoof_Nak <sikandernawaz35@gmail.com>
2023-02-14 23:39:04 +0500

    Add_Question_14

--- Question_11.js
+++ Question_11.js
@@ -1,13 +1,13 @@
 // Write a js program to count a minimum number of notes in a given amount.
 
+// SOLUTION
 // VALUES 
 let amount = 25;
-let notes = amount / 10; 
+let n_5000 = 5000;
+let n_1000 = 1000;
+let n_500 = 500;
+let n_100 = 100;
+let n_50 = 50;
+let n_10 = 10;
 
-// SOLUTION
-// USING IF CONDITION
-if (amount >= 10) {
-    console.log(parseInt(notes));
-}else{
-    console.log("Wada Note Lay K Aon");
-}
+// USING ELSE_IF CONDITION
---
+++ Question_14.js
@@ -0,0 +1,38 @@
+// Write a js program to input electricity unit charges and calculate total electricity bill according to the given condition:
+// For the first 50 units Rs. 0.50/unit
+// For the next 100 units Rs. 0.75/unit
+// For the next 100 units Rs. 1.20/unit
+// For units above 250 Rs. 1.50/unit
+// An additional surcharge of 20% is added to the bill. 
+
+// SOLUTION
+// VALUES 
+let units = 255;
+let unitPrice;
+let additonalSurcharge;
+let totalBill;
+
+if (units <= 50) {
+    let unitPrice = 0.50 * units;
+    let additonalSurcharge = (20/100) * unitPrice;
+    let totalBill = additonalSurcharge + unitPrice;
+    console.log(`Total Bill = ${totalBill}`);
+
+}else if (units > 50 && units <= 150) {
+    let unitPrice = 0.75 * units;
+    let additonalSurcharge = (20/100) * unitPrice;
+    let totalBill = additonalSurcharge + unitPrice;
+    console.log(`Total Bill = ${totalBill}`);
+
+}else if (units > 150 && units <= 250) {
+    let unitPrice = 1.20 * units;
+    let additonalSurcharge = (20/100) * unitPrice;
+    let totalBill = additonalSurcharge + unitPrice;
+    console.log(`Total Bill = ${totalBill}`);
+
+}else{
+    let unitPrice = 1.50 * units;
+    let additonalSurcharge = (20/100) * unitPrice;
+    let totalBill = additonalSurcharge + unitPrice;
+    console.log(`Total Bill = ${totalBill}`);
+}
