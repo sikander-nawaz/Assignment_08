2a1702fa1d1a25003d6e10aad18f6a9da48579eb
Khoof_Nak <sikandernawaz35@gmail.com>
2023-02-14 23:22:30 +0500

    Add_Question_13

--- Question_13.js
+++ Question_13.js
@@ -6,23 +6,26 @@
 // // SOLUTION:
 // VALUES 
 let basicSalary = 180000;
-let hra_1 = (20/100) * basicSalary;
-let da_1 = (80/100) * basicSalary;
-let grossSalary_1 = basicSalary + hra_1 + da_1;
-
-let hra_2 = (25/100) * basicSalary;
-let da_2 = (90/100) * basicSalary;
-let grossSalary_2 = basicSalary + hra_2 + da_2;
-
-let hra_3 = (30/100) * basicSalary;
-let da_3 = (95/100) * basicSalary;
-let grossSalary_3 = basicSalary + hra_3 + da_3;
+let hra;
+let da;
+let grossSalary;
 
 // // USING ELSE_IF CONDITION 
 if (basicSalary <= 10000) {
-    console.log(`Gross Salary = ${grossSalary_1}`); 
+    let hra = (20/100) * basicSalary;
+    let da = (80/100) * basicSalary;
+    let grossSalary = basicSalary + hra + da;
+    console.log(`Gross Salary = ${grossSalary}`); 
+
 }else if (basicSalary > 10000 && basicSalary <= 20000) {
-    console.log(`Gross Salary = ${grossSalary_2}`);
+    let hra = (25/100) * basicSalary;
+    let da = (90/100) * basicSalary;
+    let grossSalary = basicSalary + hra + da;
+    console.log(`Gross Salary = ${grossSalary}`);
+
 }else{
-    console.log(`Gross Salary = ${grossSalary_3}`);
+    let hra = (30/100) * basicSalary;
+    let da = (95/100) * basicSalary;
+    let grossSalary = basicSalary + hra + da;
+    console.log(`Gross Salary = ${grossSalary}`);
 }
